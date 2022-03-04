54f4da65387eba735f2671e9b839638a6be4785b
braojntu <braojntu@gmail.com>
2022-03-04 15:41:36 +0530

    App.css removed from App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
-
-import './App.css';
 
 function App() {
   return (
